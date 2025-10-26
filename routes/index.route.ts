1f6461ad2692738ef7dc44aba939962ae86f5968
QuangHaDev2004 <ngoquangha2004@gmail.com>
2025-10-26 10:47:40 +0700

    feat: api city list

--- routes/index.route.ts
+++ routes/index.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userRoutes from "./user.route";
 import authRoutes from "./auth.route";
 import companyRoutes from "./company.route";
+import cityRoutes from "./city.route";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.use("/user", userRoutes);
 router.use("/auth", authRoutes);
 
 router.use("/company", companyRoutes);
+
+router.use("/city", cityRoutes);
 
 export default router;
